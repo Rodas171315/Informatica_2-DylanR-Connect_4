36a0f2ea736b87be5280f8b5725f9efd0f9cd02a
Dylan Rodas <rodas171315@unis.edu.gt>
2018-04-19 15:07:27 -0600

    Implementando recursos de audio a js y agregando interaccion con la pagina desde js.

--- Connect-4/Programa.js
+++ Connect-4/Programa.js
@@ -1,3 +1,16 @@
+alert("Juega y\nConecta con tus amigos!");
+
+var player1 = prompt("Ingresa tu nombre jugador 1");
+var player2 = prompt("Ingresa tu nombre jugador 2");
+
+var result = confirm("Realmente quieres salir de esta pagina?");
+if (result == true) {
+  alert("Gracias por jugar");
+}
+else {
+  alert("Quieres la revancha verdad?");
+}
+
 var board = [
     [0,0,0,0,0,0],
     [0,0,0,0,0,0],
@@ -7,4 +20,8 @@ var board = [
     [0,0,0,0,0,0],
     [0,0,0,0,0,0]
 ];
+
+var winSound = new Audio('Audio/won.mp3');
+var loseSound = new Audio('Audio/lose.mp3');
+var blopSound = new Audio('Audio/blop.mp3');
 
